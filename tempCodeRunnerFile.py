8a42affeae2a649141940beee1b286951b8579d5
andresarevalo13 <afa2158@columbia.edu>
2025-05-12 15:25:13 -0400

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,29 @@
+ 1: {
+        "prompt": "Which of this sets is Dogme 95?",
+        "img1": "https://www.momendeavors.com/wp-content/uploads/2017/02/GuardiansVol2-On-Set.jpg",
+        "img2": "https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/08/idiotse.jpeg",
+        "answer": "Real Location = Dogme 95"
+    },
+    2: {
+        "prompt": "Which of these opening credits is Dogme 95?",
+        "img1": "https://lh5.googleusercontent.com/proxy/jwzeWFdIrKD_cQGQO47stuP45PGYomKqVaAIQWDFGWitX80NI-gzleKBxSUAUu_Tq1SgEHIZwzew_9zzEmvD-KTJTtEa-VXD6fiW5CLWFKyBoUuDAUolqdva6gdJ8hE45Be14ObcJiD7IwzgoVMuHFbFmU7rbndFT3hEcqU7Hw",
+        "img2": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIhFtvd2yi0fIh9gjxMOjv5M-7CAFJqEW69Q&s",
+        "answer": "No Director Credit = Dogme 95"
+    },
+    3: {
+        "prompt": "Which of this camera work is Dogme 95?",
+        "img1": "https://s.studiobinder.com/wp-content/uploads/2020/06/What-is-a-Zoom-Shot-And-What-to-Consider-When-Using-One-Featured.jpg",
+        "img2": "https://i.ytimg.com/vi/C1MqmlDeEH4/maxresdefault.jpg",
+        "answer": "Handheld Shaky Cam = Dogme 95"
+    },
+    4: {
+        "prompt": "Which of this light usage is Dogme 95?",
+        "img1": "https://i.ytimg.com/vi/sWaPAQwDS5s/sddefault.jpg",
+        "img2": "https://wdwmagic.twic.pics/ElementGalleryItems/attractions/Fullsize/Guardians-of-the-Galaxy_Full_48575.jpg?twic=v1",
+        "answer": "Natural light = Dogme 95"
+    },
+    5: {
+        "prompt": "Special Effects vs. Pure Realism (Use the slider to choose)",
+        "img1": "https://metrograph.imgix.net/2023/05/TheIdiots_Still4.jpg?fm=pjpg&ixlib=php-3.3.1",
+        "img2": "https://i.ytimg.com/vi/EIM78NrhixA/maxresdefault.jpg",
+        "answer": "Pure Realism"
